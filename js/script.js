a4b0428831e3bcb62fde484ff228a82e7c07c09f
Artur Pokrywka <pokrywkaart@gmail.com>
2019-04-04 00:00:44 +0200

    Add playerMove function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,67 @@
 "use strict";
 
-var button = document.getElementsByClassName("btn");
 var buttonRock = document.getElementById("btn-rock");
 var buttonScissors = document.getElementById("btn-scissors");
 var buttonPaper = document.getElementById("btn-paper");
+var playerChoice = ["rock", "scissors", "paper"];
 
-var number = Math.floor(Math.random() * 3) + 1;  
+function playerMove(playerChoice) {
+    var number = Math.floor(Math.random() * 3) + 1;  
+    if (number == 1) {
+        var computerMove = "paper";
+        console.log (computerMove)
+    } else if (number == 2) {
+        var computerMove = "rock";
+        console.log (computerMove)
+    } else  {
+        var computerMove = "scissors";
+        console.log (computerMove)
+    }
 
-if (number == 1) {
-    console.log ("rock")
-}
-if (number == 2){
-    console.log ("scissors")
-}
-if (number == 3) {
-    console.log ("paper")
-}
+            // sporo wątpliwości 
+    if (computerMove === playerChoice) {
+        alert("Draw");
+    }
 
+    else if (computerMove === "paper") {
+        if (playerChoice[0]) {
+            alert("Player lost");
+        }
+        else {
+            alert("Player won");
+        }
+    }
+    
+    else if (computerMove === "rock") {
+        if (playerChoice[1]) {
+            alert("Player lost");
+        }
+        else {
+            alert("Player won");
+        }
+    } 
 
-function playerMove() {
-    alert("alert");
+    else if (computerMove === "scissors") {
+        if (playerChoice[2]) {
+            alert("Player lost");
+        }
+        else {
+            alert("Player won");
+        }
+    }     
 };
 
-playerMove();
 
-button.addEventListener('click', function() {
-    playerMove();
+
+buttonRock.addEventListener('click', function() {
+    playerMove(playerChoice[0]);
+}
+);
+buttonScissors.addEventListener('click', function() {
+    playerMove(playerChoice[1]);
+}
+);
+buttonPaper.addEventListener('click', function() {
+    playerMove(playerChoice[2]);
 }
 );
